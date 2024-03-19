fd872d0b350a17dc13446e7f6283f3c11197c333
Stefanie-A <albertstephanie630@gmail.com>
2024-03-18 18:19:01 -0700

    fix: workflow modification

--- Python/database.py
+++ Python/database.py
@@ -11,8 +11,11 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USERNAME = os.environ.get("DB_USERNAME")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PORT = os.environ.get("DB_PORT")
+print(DB_PORT)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+CON = app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+print(CON)
 
 db = SQLAlchemy(app)
 
