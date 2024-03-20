441e93017a97ada2d112abb9af8cef3a5e4c622e
Stefanie-A <albertstephanie630@gmail.com>
2024-03-19 17:59:08 -0700

    feat: changing database

--- Python/app.py
+++ Python/app.py
@@ -5,7 +5,7 @@ from database import *
 app = Flask(__name__)
 task_bp =Blueprint('task', '__name__')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 db.init_app(app)
 migrate = Migrate(app, db)
 
--- Python/database.py
+++ Python/database.py
@@ -1,22 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-load_dotenv()
+#load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USERNAME = os.environ.get("DB_USERNAME")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_PORT = os.environ.get("DB_PORT")
-print(DB_PORT)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
-CON = app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-print(CON)
 
 db = SQLAlchemy(app)
+
+
+
+
+
+
+
+
+
 
 #'postgresql://username:password@localhost/database_name'
