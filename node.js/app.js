645344f07f8d331e9b20fbb0942baefed0529545
Stefanie-A <albertstephanie630@gmail.com>
2024-04-20 19:57:19 -0700

    feat: modify

--- node.js/app.js
+++ node.js/app.js
@@ -1,19 +1,23 @@
-// Importing required modules
+// We’re setting up an extremely simple server here.
 const http = require('http');
 
-// Creating a server
-const server = http.createServer((req, res) => {
-  // Set the content type of the response
-  res.setHeader('Content-Type', 'text/plain');
+// These could (should) be set as env vars.
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
 
-  // Sending "Hello, World!" as the response
-  res.end('Hello, World!\n');
-});
+// No matter what hits the server, we send the same thing.
+http.createServer((req, res) => {
 
-// Define the port to listen on
-const port = process.env.PORT || 3000;
+  // Tell the browser what’s coming.
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
 
-// Start the server and listen for incoming requests
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+  // Send a simple message in HTML.
+  res.write('<h1>I’m a Node app!</h1>');
+  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
+  res.end();
+}).listen(port, host);
+
+// This message prints in the console when the app starts.
+console.log(`App running at http://${host}:${port}`);
